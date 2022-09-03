fd3302df3fabd5a0db51f99751b567bcbc508965
beeeeeeeeeee <y.nnyrw@gmail.com>
2022-09-03 18:01:03 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import 'src/assets/css/App.css';
+
+function App() {
+	const [count, setCount] = useState(0);
+	const [loginState, setLoginState] = useState(false);
+
+	const Login = () => {};
+
+	return <div className='App'></div>;
+}
+
+export default App;
